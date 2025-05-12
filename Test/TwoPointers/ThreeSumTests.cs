4fbeb610a3aae909c3b18b8f30610997069d8cba
zkrakus <zkrakus@gmail.com>
2025-05-12 19:01:48 -0400

    feat: First pass at 3 sums. Need to get 0 values working.

---
+++ Test/TwoPointers/ThreeSumTests.cs
@@ -0,0 +1,64 @@
+﻿using neetcode.TwoPointers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.TwoPointers;
+
+public class ThreeSumTests
+{
+    [Fact]
+    public void ReturnsEmptyList_WhenInputIsNull()
+    {
+        var result = ThreeSum.Sum(null);
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void ReturnsEmptyList_WhenInputHasLessThanThreeElements()
+    {
+        var result = ThreeSum.Sum(new int[] { 1 });
+        Assert.Empty(result);
+
+        result = ThreeSum.Sum(new int[] { 1, 2 });
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void ReturnsCorrectIndices_ForSampleInput1()
+    {
+        var result = ThreeSum.Sum(new int[] { -1, 0, 1, 2, -1, -4 });
+        Assert.Contains(result, triplet => triplet.Count == 3);
+    }
+
+    [Fact]
+    public void ReturnsEmptyList_WhenNoTripletsSumToZero()
+    {
+        var result = ThreeSum.Sum(new int[] { 0, 1, 1 });
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void ReturnsEmptyList_WhenTripletSumNotZero()
+    {
+        var result = ThreeSum.Sum(new int[] { 3, -2, 1, 0 });
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void HandlesAllZeros()
+    {
+        var result = ThreeSum.Sum(new int[] { 0, 0, 0 });
+        Assert.Contains(result, triplet => triplet.Count == 3);
+    }
+
+    [Fact]
+    public void ReturnsCorrectIndices_ForValidTriplet()
+    {
+        var result = ThreeSum.Sum(new int[] { 1, 2, -3 });
+        Assert.Contains(new List<int> { 0, 1, 2 }, result);
+    }
+}
+
---
+++ neetcode/TwoPointers/ThreeSum.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neetcode.TwoPointers;
+public class ThreeSum
+{
+    public static List<List<int>> Sum(int[] nums)
+    {
+        if (nums is null || nums.Length < 3)
+            return new List<List<int>>();
+
+        List<List<int>> result = new();
+        int[] numsCopy = nums.ToArray();
+        Array.Sort(numsCopy);
+
+        int i = 0;
+        while (i < numsCopy.Length)
+        {
+            while (i < numsCopy.Length - 2 && numsCopy[i] == numsCopy[i+1])
+                i++;
+
+            int j = i + 1, k = numsCopy.Length - 1;
+            while (j < k)
+            {
+                while (j < k && numsCopy[j] == numsCopy[j + 1])
+                    j++;
+
+                while (j < k && numsCopy[k] == numsCopy[k - 1])
+                    k--;
+
+                var current = numsCopy[i] + numsCopy[j] + numsCopy[k];
+                if (current == 0)
+                {
+                    result.Add(new List<int>() { i, j, k });
+                    i++;
+                    j++;
+                }
+
+                if (current > 0)
+                    k--;
+
+                if (current < 0)
+                    j++;
+            }
+           
+
+            i++;
+        }
+
+
+        return result;
+    }
+}
