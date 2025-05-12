785e9337d7e134c7b2cc5b61211b03b80667baae
zkrakus <zkrakus@gmail.com>
2025-05-12 17:51:35 -0400

    feat: Finished TwoIntegerSum

---
+++ neetcode/Backtracking/CombinationSum.cs
@@ -0,0 +1,34 @@
+﻿namespace neetcode.Backtracking;
+public static class CombinationSum
+{
+    public static List<List<int>> DoCombinationSum(int[] nums, int target)
+    {
+        List<List<int>> result = new();
+        List<int> combination = new();
+
+        void DFS(int i, int total)
+        {
+            if(total == target)
+            {
+                result.Add(new List<int>(combination));
+                return;
+            }
+
+            if (i >= nums.Length || total > target)
+            {
+                return;
+            }
+
+            combination.Add(nums[i]);
+            DFS(i, total + nums[i]);
+
+            combination.RemoveAt(combination.Count - 1);
+            DFS(i + 1, total);
+        }
+
+        DFS(0, 0);
+
+        return result;
+    }
+
+}
---
+++ neetcode/TwoPointers/TwoIntegerSum2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neetcode.TwoPointers;
+public static class TwoIntegerSum2
+{
+    public static int[] TwoSum(int[] numbers, int target)
+    {
+        int i = 0, j = numbers.Length - 1;
+
+        while (j > i)
+        {
+            var current = numbers[i] + numbers[j];
+            if (current == target)
+                break;
+            else if (current > target)
+                j--;
+            else if(current < target)
+                i++;
+        }
+
+        return new int[] { i + 1, j + 1 };
+    }
+}
