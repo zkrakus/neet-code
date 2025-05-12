f03a87c89b8306c59af7391af7e385afd5f2a329
zkrakus <zkrakus@gmail.com>
2025-05-12 17:51:48 -0400

    feat: Finished TwoIntegerSum

---
+++ Test/Backtracking/CombinationSumTests.cs
@@ -0,0 +1,48 @@
+﻿using neetcode.Backtracking;
+
+namespace Test.Backtracking;
+public class CombinationSumTests
+{
+    [Fact]
+    public void Test_Example1()
+    {
+        var nums = new[] { 2, 5, 6, 9 };
+        var target = 9;
+        var result = CombinationSum.DoCombinationSum(nums, target);
+
+        Assert.Contains(result, r => new HashSet<int>(r).SetEquals(new[] { 2, 2, 5 }));
+        Assert.Contains(result, r => new HashSet<int>(r).SetEquals(new[] { 9 }));
+        Assert.Equal(2, result.Count);
+    }
+
+    [Fact]
+    public void Test_Example2()
+    {
+        var nums = new[] { 3, 4, 5 };
+        var target = 16;
+        var result = CombinationSum.DoCombinationSum(nums, target);
+
+        var expected = new List<List<int>> {
+            new List<int> {3,3,3,3,4},
+            new List<int> {3,3,5,5},
+            new List<int> {4,4,4,4},
+            new List<int> {3,4,4,5}
+        };
+
+        foreach (var combo in expected)
+        {
+            Assert.Contains(result, r => new HashSet<int>(r).SetEquals(combo));
+        }
+
+        Assert.Equal(4, result.Count);
+    }
+
+    [Fact]
+    public void Test_Example3()
+    {
+        var nums = new[] { 3 };
+        var target = 5;
+        var result = CombinationSum.DoCombinationSum(nums, target);
+        Assert.Empty(result);
+    }
+}
---
+++ Test/TwoPointers/TwoIntegerSum2Tests.cs
@@ -0,0 +1,17 @@
+﻿using neetcode.TwoPointers;
+
+namespace Test.TwoPointers;
+public class TwoIntegerSum2Tests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 4 }, 3, new int[] { 1, 2 })]
+    [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 1, 2 })]
+    [InlineData(new int[] { -3, -1, 0, 2, 4 }, 1, new int[] { 1, 5 })]
+    [InlineData(new int[] { 5, 25, 75 }, 100, new int[] { 2, 3 })]
+    [InlineData(new int[] { 1, 3, 4, 5, 7, 10, 11 }, 9, new int[] { 3, 4 })]
+    public void ReturnsCorrectIndices(int[] numbers, int target, int[] expected)
+    {
+        var result = TwoIntegerSum2.TwoSum(numbers, target);
+        Assert.Equal(expected, result);
+    }
+}
