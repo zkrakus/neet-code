9c08a2d5ef5287e6ff5954ff34d3a3297fc9215c
zkrakus <zkrakus@gmail.com>
2025-05-20 16:02:24 -0400

    feat:Completed TopKFrequestElements

---
+++ Test/ArraysAndHashing/TopKFrequentElementsTests.cs
@@ -0,0 +1,67 @@
+﻿using neetcode.ArraysAndHashing;
+
+namespace Test.ArraysAndHashing;
+public class TopKFrequentElementsTests
+{
+    [Fact]
+    public void TopKFrequent_BasicCase_ReturnsMostFrequent()
+    {
+        var nums = new[] { 1, 1, 1, 2, 2, 3 };
+        int k = 2;
+
+        var result = TopKFrequentElements.TopKFrequent(nums, k);
+
+        Assert.Equal(k, result.Length);
+        Assert.Contains(1, result);
+        Assert.Contains(2, result);
+    }
+
+    [Fact]
+    public void TopKFrequent_AllElementsEqualFrequency_ReturnsAnyK()
+    {
+        var nums = new[] { 4, 5, 6, 7 };
+        int k = 2;
+
+        var result = TopKFrequentElements.TopKFrequent(nums, k);
+
+        Assert.Equal(k, result.Length);
+        Assert.All(result, val => Assert.Contains(val, nums));
+    }
+
+    [Fact]
+    public void TopKFrequent_SingleElementRepeated_ReturnsThatElement()
+    {
+        var nums = new[] { 9, 9, 9, 9 };
+        int k = 1;
+
+        var result = TopKFrequentElements.TopKFrequent(nums, k);
+
+        Assert.Single(result);
+        Assert.Equal(9, result[0]);
+    }
+
+    [Fact]
+    public void TopKFrequent_KEqualsNumberOfUniques_ReturnsAll()
+    {
+        var nums = new[] { 8, 9, 10 };
+        int k = 3;
+
+        var result = TopKFrequentElements.TopKFrequent(nums, k);
+
+        Assert.Equal(3, result.Length);
+        Assert.Contains(8, result);
+        Assert.Contains(9, result);
+        Assert.Contains(10, result);
+    }
+
+    [Fact]
+    public void TopKFrequent_KIsZero_ReturnsEmpty()
+    {
+        var nums = new[] { 1, 2, 3, 4 };
+        int k = 0;
+
+        var result = TopKFrequentElements.TopKFrequent(nums, k);
+
+        Assert.Empty(result);
+    }
+}
---
+++ neetcode/ArraysAndHashing/TopKFrequentElements.cs
@@ -0,0 +1,20 @@
+﻿namespace neetcode.ArraysAndHashing;
+public static class TopKFrequentElements
+{
+    public static int[] TopKFrequent(int[] nums, int k)
+    {
+        Dictionary<int, int> elementCount = new();
+        foreach (int i in nums)
+            elementCount[i] = elementCount.GetValueOrDefault(i) + 1;
+
+        PriorityQueue<int, int> minHeap = new(Comparer<int>.Create((a,b) => b.CompareTo(a)));
+        foreach (var element in elementCount.Keys)
+            minHeap.Enqueue(element, elementCount[element]);
+
+        var result = new int[k];
+        for (int i = 0; i < k; i++)
+            result[i] = minHeap.Dequeue();
+
+        return result;
+    }
+}
