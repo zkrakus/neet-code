8d05fd7d8b3c0741511c8a62f5726fd4ab7a7378
zkrakus <zkrakus@gmail.com>
2025-05-20 17:45:50 -0400

    feat:First pass at encoding and decoding.

---
+++ Test/ArraysAndHashing/EncodingAndDecodingTests.cs
@@ -0,0 +1,70 @@
+﻿using neetcode.ArraysAndHashing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.ArraysAndHashing;
+public class EncodingAndDecodingTests
+{
+    [Fact]
+    public void EncodeDecode_RoundTrips_SimpleStrings()
+    {
+        var input = new List<string> { "hello", "cat" };
+        var encoded = EncodingAndDecoding.Encode(input);
+        var decoded = EncodingAndDecoding.Decode(encoded);
+
+        Assert.Equal(input, decoded);
+    }
+
+    [Fact]
+    public void EncodeDecode_RoundTrips_EmptyList()
+    {
+        var input = new List<string>();
+        var encoded = EncodingAndDecoding.Encode(input);
+        var decoded = EncodingAndDecoding.Decode(encoded);
+
+        Assert.Equal(input, decoded);
+        Assert.Equal(string.Empty, encoded);
+    }
+
+    [Fact]
+    public void EncodeDecode_RoundTrips_EmptyStrings()
+    {
+        var input = new List<string> { "", "", "" };
+        var encoded = EncodingAndDecoding.Encode(input);
+        var decoded = EncodingAndDecoding.Decode(encoded);
+
+        Assert.Equal(input, decoded);
+    }
+
+    [Fact]
+    public void EncodeDecode_RoundTrips_SpecialCharacters()
+    {
+        var input = new List<string> { "abc123", "x|y|z", "42|42|42" };
+        var encoded = EncodingAndDecoding.Encode(input);
+        var decoded = EncodingAndDecoding.Decode(encoded);
+
+        Assert.Equal(input, decoded);
+    }
+
+    [Fact]
+    public void Decode_HandlesSingleEncodedString()
+    {
+        string encoded = "4|test";
+        var expected = new List<string> { "test" };
+        var result = EncodingAndDecoding.Decode(encoded);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Encode_ProducesExpectedFormat()
+    {
+        var input = new List<string> { "a", "bb", "ccc" };
+        var encoded = EncodingAndDecoding.Encode(input);
+
+        Assert.Equal("1|a2|bb3|ccc", encoded);
+    }
+}
---
+++ neetcode/ArraysAndHashing/EncodingAndDecoding.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neetcode.ArraysAndHashing;
+public static class EncodingAndDecoding
+{
+    public static string Encode(IList<string> strs)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        foreach (string str in strs)
+            sb.Append($"{str.Length}|{str}");
+
+        return sb.ToString();
+    }
+
+    public static List<string> Decode(string s)
+    {
+        List<string> decodedList = new ();
+        List<char> numericChars = new List<char>();
+        for(int i = 0; i < s.Length; i++)
+        {
+            char c = s[i];
+            if (char.IsDigit(c))
+            {
+                numericChars.Add(c);
+                continue;
+            } 
+            
+            if (c == '|')
+            {
+                int stringLength = int.Parse(new string(numericChars.ToArray()));
+                numericChars.Clear();
+
+                int startIndex = i + 1;
+                var decodedStr = s.Substring(startIndex, stringLength);
+                decodedList.Add(decodedStr);
+
+                i = startIndex + stringLength - 1;
+            }
+        }
+
+        return decodedList;
+    }
+}
