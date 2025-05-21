ef9db68c909fdc8c010d02c8963819a9a47c99df
zkrakus <zkrakus@gmail.com>
2025-05-21 00:24:33 -0400

    feat:ReversePolishNotation problem complete with tests. East one.

---
+++ Test/Stack/EvaluateReversePolishNotationTests.cs
@@ -0,0 +1,66 @@
+﻿using neetcode.Stack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Stack;
+public class EvaluateReversePolishNotationTests
+{
+    [Fact]
+    public void EvalRPN_Addition_ReturnsCorrectResult()
+    {
+        var tokens = new[] { "2", "3", "+" };
+        int result = EvaluateReversePolishNotation.EvalRPN(tokens);
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void EvalRPN_Subtraction_ReturnsCorrectResult()
+    {
+        var tokens = new[] { "10", "4", "-" };
+        int result = EvaluateReversePolishNotation.EvalRPN(tokens);
+        Assert.Equal(6, result);
+    }
+
+    [Fact]
+    public void EvalRPN_Multiplication_ReturnsCorrectResult()
+    {
+        var tokens = new[] { "2", "3", "*" };
+        int result = EvaluateReversePolishNotation.EvalRPN(tokens);
+        Assert.Equal(6, result);
+    }
+
+    [Fact]
+    public void EvalRPN_Division_ReturnsCorrectResult()
+    {
+        var tokens = new[] { "8", "2", "/" };
+        int result = EvaluateReversePolishNotation.EvalRPN(tokens);
+        Assert.Equal(4, result);
+    }
+
+    [Fact]
+    public void EvalRPN_MixedExpression_ReturnsCorrectResult()
+    {
+        var tokens = new[] { "2", "1", "+", "3", "*" }; // (2 + 1) * 3 = 9
+        int result = EvaluateReversePolishNotation.EvalRPN(tokens);
+        Assert.Equal(9, result);
+    }
+
+    [Fact]
+    public void EvalRPN_ComplexExpression_ReturnsCorrectResult()
+    {
+        var tokens = new[] { "4", "13", "5", "/", "+" }; // 4 + (13 / 5) = 6
+        int result = EvaluateReversePolishNotation.EvalRPN(tokens);
+        Assert.Equal(6, result);
+    }
+
+    [Fact]
+    public void EvalRPN_SingleValue_ReturnsThatValue()
+    {
+        var tokens = new[] { "42" };
+        int result = EvaluateReversePolishNotation.EvalRPN(tokens);
+        Assert.Equal(42, result);
+    }
+}
---
+++ neetcode/Stack/EvaluateReversePolishNotation.cs
@@ -0,0 +1,44 @@
+﻿namespace neetcode.Stack;
+public static class EvaluateReversePolishNotation
+{
+
+    public static int EvalRPN(string[] tokens)
+    {
+        Stack<int> _stack = new();
+        foreach (var token in tokens)
+        {
+            if (int.TryParse(token, out var value))
+            {
+                _stack.Push(value);
+            }
+            else
+            {
+                switch (token)
+                {
+                    case "+":
+                        _stack.Push(_stack.Pop() + _stack.Pop());
+                        break;
+                    case "-":
+                        {
+                            int x = _stack.Pop(), y = _stack.Pop();
+                            _stack.Push(y - x);
+                            break;
+                        }
+                    case "*":
+                        _stack.Push(_stack.Pop() * _stack.Pop());
+                        break;
+                    case "/":
+                        {
+                            int x = _stack.Pop(), y = _stack.Pop();
+                            _stack.Push(y / x);
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+        }
+
+        return _stack.Pop();
+    }
+}
