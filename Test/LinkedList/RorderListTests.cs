7619af316706a8027f7b32f6b65e27cf012b6de8
zkrakus <zkrakus@gmail.com>
2025-05-31 20:46:08 -0400

    feat: Did reorder list w/ tests.

---
+++ Test/LinkedList/RorderListTests.cs
@@ -0,0 +1,54 @@
+﻿using neetcode.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.LinkedList;
+public class RorderListTests
+{
+    private ListNode CreateList(params int[] values)
+    {
+        if (values.Length == 0) return null!;
+        var head = new ListNode(values[0]);
+        var current = head;
+        for (int i = 1; i < values.Length; i++)
+        {
+            current.next = new ListNode(values[i]);
+            current = current.next;
+        }
+        return head;
+    }
+
+    private List<int> ToList(ListNode? head)
+    {
+        var result = new List<int>();
+        while (head != null)
+        {
+            result.Add(head.val);
+            head = head.next;
+        }
+        return result;
+    }
+
+    [Theory]
+    [InlineData(new int[] { }, new int[] { })]
+    [InlineData(new int[] { 1 }, new int[] { 1 })]
+    [InlineData(new int[] { 1, 2 }, new int[] { 1, 2 })]
+    [InlineData(new int[] { 1, 2, 3 }, new int[] { 1, 3, 2 })]
+    [InlineData(new int[] { 1, 2, 3, 4 }, new int[] { 1, 4, 2, 3 })]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 5, 2, 4, 3 })]
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6 }, new int[] { 1, 6, 2, 5, 3, 4 })]
+    public void Reorder_ReordersListCorrectly(int[] input, int[] expected)
+    {
+        // Arrange
+        var head = CreateList(input);
+
+        // Act
+        var reordered = ReorderList.Reorder(head);
+
+        // Assert
+        Assert.Equal(expected, ToList(reordered));
+    }
+}
--- neetcode/LinkedList/ReorderList.cs
+++ neetcode/LinkedList/ReorderList.cs
@@ -1,15 +1,44 @@
 ﻿namespace neetcode.LinkedList;
 public static class ReorderList
 {
-    //public static void Reorder(ListNode head)
-    //{
-    //    ListNode
+    public static ListNode? Reorder(ListNode? head)
+    {
+        if (head is null || head.next is null)
+            return head;
 
-    //    static ListNode ReverseLinkedListRec(ListNode? cur, ListNode? prev)
-    //    {
+        ListNode? t = head;
+        ListNode? h = head?.next;
+        while (h?.next != null)
+        {
+            t = t?.next;
+            h = h?.next?.next;
+        }
 
-    //    }
+        h = t!.next; // The new right pointer is now the beginning of the secondary linked list.
+        t.next = null; // Sever the pointer from the first linked list from the second.
+        static ListNode? ReverseLinkedListRec(ListNode? prev, ListNode? cur)
+        {
+            if (cur is null)
+                return prev!;
 
-    //    reversedHead = ReverseLinkedListRec(reverseHead, reversePrev);
-    //}
+            var tmp = cur.next;
+            cur.next = prev;
+
+            return ReverseLinkedListRec(cur, tmp); // prev = cur, cur = tmp;
+        }
+
+        h = ReverseLinkedListRec(null, h);
+
+        // Merge the two halves: first half (starting at 'head') and reversed second half (starting at 'h')
+        t = head;
+        while (t is not null && h is not null)
+        {
+            var tmp = t.next;
+            t.next = h;
+            t = h;
+            h = tmp;
+        }
+
+        return head!;
+    }
 }
