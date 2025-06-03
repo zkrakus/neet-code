56f2ac03f1da40e1cbb0069d14ea56bcba8c3ff1
zkrakus <zkrakus@gmail.com>
2025-06-03 12:29:23 -0400

    feat: cleaned up balanced and diameter of binary tree.

--- neetcode/Trees/BalancedBinaryTree.cs
+++ neetcode/Trees/BalancedBinaryTree.cs
@@ -3,43 +3,94 @@
 namespace neetcode.Trees;
 public static class BalancedBinaryTree
 {
+    //public static bool IsBalanced(TreeNode? root)
+    //{
+    //    if (root is null)
+    //        return true;
+
+    //    var height = 0;
+    //    var leftHeight = DFSHeight(root.left, height);
+    //    var rightHeight = DFSHeight(root.right, height);
+
+    //    if (!leftHeight.balanced || !rightHeight.balanced)
+    //        return false;
+
+    //    var heightDifference = Math.Abs(rightHeight.height - leftHeight.height);
+    //    if (heightDifference > 1)
+    //        return false;
+    //    else
+    //        return true;
+    //}
+
+    //private static (int height, bool balanced) DFSHeight(TreeNode? node, int height)
+    //{
+    //    if (node is null)
+    //        return (height, true);
+
+    //    height++;
+
+    //    var leftHeight = DFSHeight(node.left, height);
+    //    var rightHeight = DFSHeight(node.right, height);
+    //    var maxheight = Math.Max(leftHeight.height, rightHeight.height);
+
+    //    if (!leftHeight.balanced || !rightHeight.balanced)
+    //        return (height, false);
+
+    //    var heightDifference = Math.Abs(rightHeight.height - leftHeight.height);
+    //    if (heightDifference > 1)
+    //        return (maxheight, false);
+    //    else
+    //        return (maxheight, true);
+    //}
+
+
+
     public static bool IsBalanced(TreeNode? root)
     {
-        if (root is null)
-            return true;
+        bool isBalanced = true;
+        int IsBalancedRec(TreeNode node, int depth)
+        {
+            if (node == null || isBalanced == false)
+                return 0;
 
-        var height = 0;
-        var leftHeight = DFSHeight(root.left, height);
-        var rightHeight = DFSHeight(root.right, height);
+            var ld = IsBalancedRec(node.left, depth);
+            var rd = IsBalancedRec(node.right, depth);
 
-        if (!leftHeight.balanced || !rightHeight.balanced)
-            return false;
+            if (Math.Abs(ld - rd) > 1)
+                isBalanced = false;
 
-        var heightDifference = Math.Abs(rightHeight.height - leftHeight.height);
-        if (heightDifference > 1)
-            return false;
-        else
-            return true;
+            var greaterDepth = Math.Max(ld, rd);
+
+            return ++greaterDepth;
+        }
+
+
+        _ = IsBalancedRec(root, 0);
+
+        return isBalanced;
     }
 
-    private static (int height, bool balanced) DFSHeight(TreeNode? node, int height)
-    {
-        if (node is null)
-            return (height, true);
 
-        height++;
 
-        var leftHeight = DFSHeight(node.left, height);
-        var rightHeight = DFSHeight(node.right, height);
-        var maxheight = Math.Max(leftHeight.height, rightHeight.height);
 
-        if (!leftHeight.balanced || !rightHeight.balanced)
-            return (height, false);
 
-        var heightDifference = Math.Abs(rightHeight.height - leftHeight.height);
-        if (heightDifference > 1)
-            return (maxheight, false);
-        else
-            return (maxheight, true);
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- neetcode/Trees/DiameterOfBinaryTree.cs
+++ neetcode/Trees/DiameterOfBinaryTree.cs
@@ -9,33 +9,6 @@ using System.Xml.Linq;
 namespace neetcode.Trees;
 public class DiameterOfBinaryTree
 {
-    //public int diameter { get; set; } = 0;
-
-    //public int FindDiameter(TreeNode? root)
-    //{
-    //    if (root is null)
-    //        return 0;
-
-    //    FindHeightRecursive(root);
-
-    //    return diameter;
-    //}
-
-    //public int FindHeightRecursive(TreeNode? node)
-    //{
-    //    if (node is null)
-    //        return 0;
-
-    //    var leftHeight = FindHeightRecursive(node.left);
-    //    var rightHeight = FindHeightRecursive(node.right);
-
-    //    this.diameter = Math.Max(this.diameter, leftHeight + rightHeight);
-
-    //    return 1 + Math.Max(leftHeight, rightHeight);
-    //}
-
-
-
     public int FindDiameter(TreeNode? root)
     {
         int maxDiameter = 0;
