25e628670dec562194b80729d16cbff1cb87ac07
zkrakus <zkrakus@gmail.com>
2025-06-03 12:17:41 -0400

    feat: unique solution for DiameterOfBinaryTree.

--- neetcode/Trees/DiameterOfBinaryTree.cs
+++ neetcode/Trees/DiameterOfBinaryTree.cs
@@ -9,28 +9,72 @@ using System.Xml.Linq;
 namespace neetcode.Trees;
 public class DiameterOfBinaryTree
 {
-    public int diameter { get; set; } = 0;
+    //public int diameter { get; set; } = 0;
+
+    //public int FindDiameter(TreeNode? root)
+    //{
+    //    if (root is null)
+    //        return 0;
+
+    //    FindHeightRecursive(root);
+
+    //    return diameter;
+    //}
+
+    //public int FindHeightRecursive(TreeNode? node)
+    //{
+    //    if (node is null)
+    //        return 0;
+
+    //    var leftHeight = FindHeightRecursive(node.left);
+    //    var rightHeight = FindHeightRecursive(node.right);
+
+    //    this.diameter = Math.Max(this.diameter, leftHeight + rightHeight);
+
+    //    return 1 + Math.Max(leftHeight, rightHeight);
+    //}
+
+
 
     public int FindDiameter(TreeNode? root)
     {
-        if (root is null)
-            return 0;
+        int maxDiameter = 0;
+        int DiameterOfBinaryTreeRec2(TreeNode? node, int depth)
+        {
+            if (node is null)
+                return 0;
 
-        FindHeightRecursive(root);
+            var lh = DiameterOfBinaryTreeRec2(node.left, 0);
+            var rh = DiameterOfBinaryTreeRec2(node.right, 0);
 
-        return diameter;
+            var greaterDepth = Math.Max(lh, rh);
+            maxDiameter = Math.Max(maxDiameter, lh + rh);
+
+            return greaterDepth + 1;
+        }
+
+        _ = DiameterOfBinaryTreeRec2(root, maxDiameter);
+
+        return maxDiameter;
     }
 
-    public int FindHeightRecursive(TreeNode? node)
-    {
-        if (node is null)
-            return 0;
 
-        var leftHeight = FindHeightRecursive(node.left);
-        var rightHeight = FindHeightRecursive(node.right);
-        
-        this.diameter = Math.Max(this.diameter, leftHeight + rightHeight);
 
-        return 1 + Math.Max(leftHeight, rightHeight);
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
