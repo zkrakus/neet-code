aa1093914662a6c5cb5909b121a7eeeea95572c5
zkrakus <zkrakus@gmail.com>
2025-06-13 10:30:39 -0400

    feat: First pass at permutations. Took a while to complete solution. Need to review and try again for a better time.

---
+++ neetcode/Backtracking/Permutations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neetcode.Backtracking;
+public static class Permutations
+{
+    public static List<List<int>> Permute(int[] nums)
+    {
+        if (nums is null || nums.Length == 0)
+            return new List<List<int>>();
+
+        var permutations = Permute(nums[1..]);
+        var result = new List<List<int>>();
+        foreach (var permutation in permutations)
+        {
+            for (int i = 0; i <= permutation.Count; i++)
+            {
+                var p_copy = permutation.ToList();
+                p_copy.Insert(i, nums[0]);
+                result.Add(p_copy);
+            }
+        }
+
+        return result;
+    }
+}
