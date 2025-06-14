0a45b28ac54079afcdd5b4486a9ea8b70ac95cc8
zkrakus <zkrakus@gmail.com>
2025-06-14 10:39:29 -0400

    feat: Working through a backtracking solution.

--- neetcode/Backtracking/Subsets.cs
+++ neetcode/Backtracking/Subsets.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace neetcode.Backtracking;
+﻿namespace neetcode.Backtracking;
 public static class Subsets
 {
     public static List<List<int>>? FindSubsets(int[] nums)
@@ -14,9 +6,9 @@ public static class Subsets
         if (nums is null)
             return null;
 
-        List<List<int>> result = new List<List<int>>();
-        List<int> subset = new List<int>();
-        void FindSubsetsRec(int i, List<int> subset)
+        List<List<int>> result = new();
+        List<int> subset = new();
+        void FindSubsetsRec(int i)
         {
             if (i == nums.Length)
             {
@@ -25,13 +17,49 @@ public static class Subsets
             }
 
             subset.Add(nums[i]);
-            FindSubsetsRec(i + 1, subset);
+            FindSubsetsRec(i + 1);
             subset.RemoveAt(subset.Count - 1);
-            FindSubsetsRec(i + 1, subset);
+            FindSubsetsRec(i + 1);
         }
 
-        FindSubsetsRec(0, subset);
+        FindSubsetsRec(0);
 
         return result;
+    }
+
+    public static IList<IList<int>> SubsetsIterative(int[] nums)
+    {
+        var result = new List<IList<int>> { new List<int>() };
+
+        foreach (int num in nums)
+        {
+            int count = result.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var newSubset = new List<int>(result[i]) { num };
+                result.Add(newSubset);
+            }
+        }
+
+        return result;
+    }
+
+    public static IList<IList<int>> SubsetsBacktracking(int[] nums)
+    {
+        var result = new List<IList<int>>();
+        Backtrack(0, new List<int>());
+        return result;
+
+        void Backtrack(int start, List<int> path)
+        {
+            result.Add(new List<int>(path));
+
+            for (int i = start; i < nums.Length; i++)
+            {
+                path.Add(nums[i]);
+                Backtrack(i + 1, path);
+                path.RemoveAt(path.Count - 1); // undo step
+            }
+        }
     }
 }
