7d759683450be9fc7d141b26656f63aaf9e14cf4
zkrakus <zkrakus@gmail.com>
2025-06-17 16:30:57 -0400

    feat:Did subsets2 first time seeing the problem solo.

---
+++ Test/Backtracking/Subsets2Tests.cs
@@ -0,0 +1,78 @@
+﻿using neetcode.Backtracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Backtracking;
+public class Subsets2Tests
+{
+    [Fact]
+    public void SubsetsWithDup_ReturnsEmptySubset_WhenInputIsEmpty()
+    {
+        var result = Subsets2.SubsetsWithDup(new int[] { });
+
+        Assert.Single(result);
+        Assert.Contains(result, r => r.Count == 0);
+    }
+
+    [Fact]
+    public void SubsetsWithDup_HandlesSingleElement()
+    {
+        var result = Subsets2.SubsetsWithDup(new[] { 1 });
+
+        var expected = new List<List<int>>
+            {
+                new List<int>(),       // []
+                new List<int>{1},      // [1]
+            };
+
+        Assert.Equal(expected.Count, result.Count);
+        Assert.All(expected, subset => Assert.Contains(result, r => r.SequenceEqual(subset)));
+    }
+
+    [Fact]
+    public void SubsetsWithDup_HandlesTwoDistinctElements()
+    {
+        var result = Subsets2.SubsetsWithDup(new[] { 1, 2 });
+
+        var expected = new List<List<int>>
+            {
+                new List<int>(),       // []
+                new List<int>{1},      // [1]
+                new List<int>{2},      // [2]
+                new List<int>{1,2},    // [1,2]
+            };
+
+        Assert.Equal(expected.Count, result.Count);
+        Assert.All(expected, subset => Assert.Contains(result, r => r.SequenceEqual(subset)));
+    }
+
+    [Fact]
+    public void SubsetsWithDup_HandlesDuplicatesCorrectly()
+    {
+        var result = Subsets2.SubsetsWithDup(new[] { 1, 2, 2 });
+
+        var expected = new List<List<int>>
+            {
+                new List<int>(),       // []
+                new List<int>{1},      // [1]
+                new List<int>{2},      // [2]
+                new List<int>{1,2},    // [1,2]
+                new List<int>{2,2},    // [2,2]
+                new List<int>{1,2,2},  // [1,2,2]
+            };
+
+        Assert.Equal(expected.Count, result.Count);
+        Assert.All(expected, subset => Assert.Contains(result, r => r.SequenceEqual(subset)));
+    }
+
+    [Fact]
+    public void SubsetsWithDup_HandlesNullInput()
+    {
+        var result = Subsets2.SubsetsWithDup(null);
+
+        Assert.Empty(result);
+    }
+}
--- neetcode/Backtracking/Subsets2.cs
+++ neetcode/Backtracking/Subsets2.cs
@@ -9,12 +9,32 @@ public static class Subsets2
 {
     public static List<List<int>> SubsetsWithDup(int[] nums)
     {
-        if (nums is null || nums.Length == 0)
-            return new List<List<int>>() { new List<int>() };
+        if (nums is null)
+            return new List<List<int>>() { };
 
-        if(nums.Length == 1)
-            return new List<List<int>>() { new List<int> { nums[0] } };
+        List<int> numsCopy = nums.ToList();
+        numsCopy.Sort();
+        List<List<int>> result = new();
+        List<int> subset = new();
+        void Backtrack(int i = 0)
+        {
+            if (i >= numsCopy.Count)
+            {
+                result.Add(subset.ToList());
+                return;
+            }
 
+            subset.Add(numsCopy[i]);
+            Backtrack(i + 1);
 
+            subset.RemoveAt(subset.Count - 1);
+            while (i + 1 < numsCopy.Count && numsCopy[i] == numsCopy[i + 1]) i++;
+
+            Backtrack(i + 1);
+        }
+
+        Backtrack();
+
+        return result;
     }
 }
