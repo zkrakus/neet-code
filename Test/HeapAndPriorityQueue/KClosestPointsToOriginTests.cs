9065c7d70d62fca8e2c1c25822994e4ab28f67bc
zkrakus <zkrakus@gmail.com>
2025-06-18 12:08:21 -0400

    feat:Finished KClosestPoints to origin.

---
+++ Test/HeapAndPriorityQueue/KClosestPointsToOriginTests.cs
@@ -0,0 +1,90 @@
+﻿using neetcode.HeapAndPriorityQueue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.HeapAndPriorityQueue;
+public class KClosestPointsToOriginTests
+{
+    [Fact]
+    public void KClosest_ReturnsEmptyArray_WhenKIsZero()
+    {
+        int[][] points = new int[][]
+        {
+                new int[] { 1, 2 },
+                new int[] { 3, 4 }
+        };
+
+        var result = KClosestPointsToOrigin.KClosest(points, 0);
+
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void KClosest_ReturnsAllPoints_WhenKEqualsPointsCount()
+    {
+        int[][] points = new int[][]
+        {
+                new int[] { 1, 2 },
+                new int[] { 3, 4 }
+        };
+
+        var result = KClosestPointsToOrigin.KClosest(points, 2);
+
+        Assert.Equal(2, result.Length);
+        Assert.Contains(result, p => p[0] == 1 && p[1] == 2);
+        Assert.Contains(result, p => p[0] == 3 && p[1] == 4);
+    }
+
+    [Fact]
+    public void KClosest_ReturnsSingleClosestPoint()
+    {
+        int[][] points = new int[][]
+        {
+                new int[] { 5, 5 },
+                new int[] { 2, 1 },
+                new int[] { -1, -1 }
+        };
+
+        var result = KClosestPointsToOrigin.KClosest(points, 1);
+
+        Assert.Single(result);
+        Assert.True(result[0][0] == -1 && result[0][1] == -1 || result[0][0] == 2 && result[0][1] == 1);
+    }
+
+    [Fact]
+    public void KClosest_ReturnsKClosestPoints_RegardlessOfOrder()
+    {
+        int[][] points = new int[][]
+        {
+                new int[] { 1, 3 },
+                new int[] { -2, 2 },
+                new int[] { 5, 8 },
+                new int[] { 0, 1 }
+        };
+
+        var result = KClosestPointsToOrigin.KClosest(points, 2);
+
+        Assert.Equal(2, result.Length);
+        Assert.Contains(result, p => p[0] == -2 && p[1] == 2);
+        Assert.Contains(result, p => p[0] == 0 && p[1] == 1);
+    }
+
+    [Fact]
+    public void KClosest_ReturnsCorrectly_WhenAllPointsSameDistance()
+    {
+        int[][] points = new int[][]
+        {
+                new int[] { 1, 1 },
+                new int[] { -1, -1 },
+                new int[] { 1, -1 },
+                new int[] { -1, 1 }
+        };
+
+        var result = KClosestPointsToOrigin.KClosest(points, 2);
+
+        Assert.Equal(2, result.Length);
+    }
+}
---
+++ neetcode/HeapAndPriorityQueue/KClosestPointsToOrigin.cs
@@ -0,0 +1,34 @@
+﻿namespace neetcode.HeapAndPriorityQueue;
+public class KClosestPointsToOrigin
+{
+    public static int[][] KClosest(int[][] points, int k)
+    {
+        if (k <= 0) return Array.Empty<int[]>();
+
+        //PriorityQueue<(int[] point, int priority), int> minHeap = new(Comparer<int>.Create((a, b) => a.CompareTo(b)));
+        PriorityQueue<(int[] point, int priority), int> maxHeap = new(Comparer<int>.Create((a, b) => b.CompareTo(a)));
+        foreach (var point in points)
+        {
+            // We technically can just use a **2 = b**2 because we care about relative distance by priority not the distance itself.
+            // Using the distance itself will fuck shit up because of the folloring operation. We would have to use decimals or doubles.
+            var distanceSquared = (point[0] * point[0]) + (point[1] * point[1]);
+
+            // insert anything that's smaller than the smallest number. Will need to dequeue k, but minHeap will likely be larger than k up to a max of n.
+            //if (minHeap.Count < k || distanceFromOrigin <= minHeap.Peek().priority)
+            //    minHeap.Enqueue((point, distanceFromOrigin), distanceFromOrigin);
+
+            // use max heap instead. Now we know the largest element k elements that we iterated over, so we will pop that largest element if we find a closer point, but keep size k.
+            if (maxHeap.Count < k)
+            {
+                maxHeap.Enqueue((point, distanceSquared), distanceSquared);
+            }
+            else if (distanceSquared < maxHeap.Peek().priority)
+            {
+                maxHeap.Dequeue();
+                maxHeap.Enqueue((point, distanceSquared), distanceSquared);
+            }
+        }
+
+        return Enumerable.Range(0, k).Select(_ => maxHeap.Dequeue().point).ToArray();
+    }
+}
