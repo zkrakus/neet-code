0e6ce9d6f67b8a96790ab129d7197db1df67fa37
zkrakus <zkrakus@gmail.com>
2025-07-04 12:51:02 -0400

    feat:Did greedy. Try other solutions types such as dp bottom up.

---
+++ neetcode/Greedy/MaximumSubarray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neetcode.Greedy;
+public static class MaximumSubarray
+{
+    public static int MaxSubArray(int[] nums)
+    {
+        int maxSub = nums[0], curSum = 0;
+        foreach (int num in nums)
+        {
+            if(curSum < 0) {
+                curSum = 0;
+            }
+
+            curSum += num;
+            maxSub = Math.Max(maxSub, curSum);
+        }
+
+        return maxSub;
+    }
+}
