1bd7ea5b095c8857a0a3f2ebe5653d0609e61bb4
zkrakus <zkrakus@gmail.com>
2025-08-05 15:38:32 -0400

    feat:Did MinCost brute recur and dp recur from scatch. Starting the get the hang of dp.

--- neetcode/OneDimensionalDynamicProgramming/ClimbingStairs.cs
+++ neetcode/OneDimensionalDynamicProgramming/ClimbingStairs.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,12 +14,12 @@ public static class ClimbingStairs
             return 0;
         if (n == 0)
             return 1;
-        if (n == 1) 
+        if (n == 1)
             return 1;
 
-        int nMinus1 = 1; 
+        int nMinus1 = 1;
         int nMinus2 = 1;
-        for (int i = 2; i <= n; i ++)
+        for (int i = 2; i <= n; i++)
         {
             var tmp = nMinus1;
             nMinus1 += nMinus2;
@@ -30,6 +31,9 @@ public static class ClimbingStairs
 
     public static int ClimbStairsBrute(int n)
     {
+        if (n < 0)
+            return 0;
+
         static int ClimbStairsRecursive(int cur, int n)
         {
             if (cur == n) return 1;
@@ -43,22 +47,21 @@ public static class ClimbingStairs
 
     public static int ClimbMemoized(int n)
     {
+        if (n < 0)
+            return 0;
+
         Dictionary<int, int> dp = new();
-        return ClimbStairsMemoizedRecursive(0, n, dp);
-    }
+        int ClimbMemoizedRecursive(int n, int cur, Dictionary<int, int> dp)
+        {
+            if (cur > n) return 0;
+            if (cur == n) return 1;
+            if (dp.TryGetValue(cur, out var output)) return output;
 
-    public static int ClimbStairsMemoizedRecursive(int cur, int n, Dictionary<int, int> dp)
-    {
-        if (cur == n) return 1;
-        if (cur > n) return 0;
+            dp[cur] = ClimbMemoizedRecursive(n, cur + 1, dp) + ClimbMemoizedRecursive(n, cur + 2, dp);
 
-        if (dp.TryGetValue(cur, out int value))
-            return value;
+            return dp[cur];
+        }
 
-        int result = ClimbStairsMemoizedRecursive(cur + 1, n, dp)
-               + ClimbStairsMemoizedRecursive(cur + 2, n, dp);
-
-        dp[cur] = result;
-        return result;
+        return ClimbMemoizedRecursive(n, 0, dp);
     }
 }
---
+++ neetcode/OneDimensionalDynamicProgramming/MinConstClimbingStairs.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.ConstrainedExecution;
+
+namespace neetcode.OneDimensionalDynamicProgramming;
+public static class MinConstClimbingStairs
+{
+    public static int MinCost(int[] cost)
+    {
+        if (cost == null || cost.Length == 0) return 0;
+        Dictionary<int, int> dp = new();
+
+        int MinCostRecur(int cur)
+        {
+            if (cur >= cost.Length) return 0; // Once you pass the last step, cost is 0
+            if (dp.TryGetValue(cur, out int cached)) return cached;
+
+            var min = cost[cur] + Math.Min(MinCostRecur(cur + 1), MinCostRecur(cur + 2));
+            dp[cur] = min;
+
+            return min;
+        }
+
+        return Math.Min(MinCostRecur(0), MinCostRecur(1));
+    }
+
+
+    public static int MinCostRec(int[] cost)
+    {
+        if (cost is null || cost.Length == 0) return 0;
+
+        int MinCostRecurrenceFunction(int cur)
+        {
+            if (cur >= cost.Length) return 0;
+
+            return cost[cur] + Math.Min(MinCostRecurrenceFunction(cur + 2), MinCostRecurrenceFunction(cur + 1));
+        }
+
+        return Math.Min(MinCostRecurrenceFunction(1), MinCostRecurrenceFunction(0));
+    }
+}
