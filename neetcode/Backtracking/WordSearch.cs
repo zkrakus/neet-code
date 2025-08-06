31090a5b54a65c8bffb13af488db5c49d0f43de4
zkrakus <zkrakus@gmail.com>
2025-08-06 17:43:16 -0400

    feat:Did work search. Getting easier. Could use a hash to mark the board instead of adding to path, but who cares, not worth it.

--- neetcode/Backtracking/WordSearch.cs
+++ neetcode/Backtracking/WordSearch.cs
@@ -1,45 +1,41 @@
 ﻿namespace neetcode.Backtracking;
 public static class WordSearch
 {
-    // O(n * m * 4^word.len) 
-    public static bool Exist(char[][] board, string word)
+    // O(m * n * 4^word.len) 
+    // S(word.len)
+    public static bool Exists(char[][] board, string word)
     {
-        int rows = board.Length, cols = board[0].Length;
-        var path = new HashSet<(int, int)>();
+        if (board is null || board.Length == 0 || board[0].Length == 0 || word is null || word.Length == 0)
+            return false;
 
+
+        int rows = board.Length, cols = board[0].Length;
+        HashSet<(int, int)> path = new();
         bool dfs(int row, int col, int i)
         {
             if (i == word.Length)
-            {
                 return true;
-            }
-
-            if (
-                (row < 0 || col < 0 || row >= rows || col >= cols) // Out of bounds
-                || word[i] != board[row][col] // Not the character we are searching for.
-                || !path.Contains((row, col))
-                )
-            {
-                return false;
-            }
 
             path.Add((row, col));
-            var res = (dfs(row + 1, col, i + 1) ||
-                dfs(row - 1, col, i + 1) ||
-                dfs(row, col, i + 1) ||
-                dfs(row, col + 1, i + 1) ||
-                dfs(row, col - 1, i + 1));
+            if (row >= rows || col >= cols || row < 0 || col < 0 || // Bounds checks
+                board[row][col] != word[i] || // Character comparison
+                path.Contains((row, col))) // Check we aren't going back to a position we have been at already.
+                return false;
 
+            var res = dfs(row, col + 1, i + 1) || dfs(row, col - 1, i + 1) || dfs(row + 1, col, i + 1) || dfs(row - 1, col, i + 1); // Check other positions dfs style.
             path.Remove((row, col));
 
             return res;
         }
 
-        foreach (var row in Enumerable.Range(0, rows - 1))
-            foreach (var col in Enumerable.Range(0, cols - 1))
-                if (dfs(row, col, 0))
-                    return true;
+        //foreach (var row in Enumerable.Range(0, rows))
+        //    foreach (var col in Enumerable.Range(0, cols))
+        //        if (dfs(row, col, 0))
+        //            return true;
+        //return false;
 
-        return false;
+        return Enumerable.Range(0, rows)
+            .SelectMany((row) => Enumerable.Range(0, cols), (row, col) => (row, col)).
+            Any(rc => dfs(rc.row, rc.col, 0));
     }
 }
