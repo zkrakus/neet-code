5be519da16cac9353c4352d5b7e05b00f7c13e65
zkrakus <zkrakus@gmail.com>
2025-08-13 13:17:08 -0400

    feat:New solution for valid sudoku. Used hashsets and didn't use recursion. Also learned trick for calulating sub squares of matrixes

--- neetcode/ArraysAndHashing/ValidSudoku.cs
+++ neetcode/ArraysAndHashing/ValidSudoku.cs
@@ -1,7 +1,9 @@
-﻿namespace neetcode.ArraysAndHashing;
+﻿using System.Runtime.ExceptionServices;
+
+namespace neetcode.ArraysAndHashing;
 public static class ValidSudoku
 {
-    public bool IsValidSudoku(char[][] board)
+    public static bool IsValidSudoku(char[][] board)
     {
         int rows = board.Length, cols = board[0].Length;
         char emptyCell = '.';
@@ -33,6 +35,50 @@ public static class ValidSudoku
             foreach (var col in Enumerable.Range(0, cols))
                 if (!IsValidSudo(row, col))
                     return false;
+
+        return true;
+    }
+
+    public static bool IsValidSudoku2(char[][] board)
+    {
+        for (int row = 0; row < 9; row++)
+        {
+            HashSet<char> seen = new();
+            for (int c = 0; c < 9; c++)
+            {
+                if (board[row][c] == '.') continue;
+                if (seen.Contains(board[row][c])) return false;
+                seen.Add(board[row][c]);
+            }
+        }
+
+
+        for (int col = 0; col < 9; col++)
+        {
+            HashSet<char> seen = new();
+            for (int r = 0; r < 9; r++)
+            {
+                if (board[r][col] == '.') continue;
+                if (seen.Contains(board[r][col])) return false;
+                seen.Add(board[r][col]);
+            }
+        }
+
+        for (int square = 0; square < 9; square++)
+        {
+            HashSet<char> seen = new();
+            for (int m = 0; m < 3; m++)
+            {
+                for (int n = 0; n < 3; n++)
+                {
+                    int col = (((square / 3) * 3)) + m;
+                    int row = (((square % 3) * 3)) + n;
+                    if (board[row][col] == '.') continue;
+                    if (seen.Contains(board[row][col])) return false;
+                    seen.Add(board[row][col]);
+                }
+            }
+        }
 
         return true;
     }
