26877898aa7a744e742018915e0bcbf860d896cc
zkrakus <zkrakus@gmail.com>
2025-08-13 14:35:59 -0400

    feat:Damn you got the two pointers fast.

--- neetcode/TwoPointers/ContainerWithMostWater.cs
+++ neetcode/TwoPointers/ContainerWithMostWater.cs
@@ -1,7 +1,7 @@
 ﻿namespace neetcode.TwoPointers;
 public static class ContainerWithMostWater
 {
-    public static int MaxArea(int[] heights)
+    public static int MaxAreaBrute(int[] heights)
     {
         if (heights is null || heights.Length == 0) return 0;
 
@@ -9,6 +9,22 @@ public static class ContainerWithMostWater
         for (int i = 0; i < heights.Length; i++)
             for (int j = i + 1; j < heights.Length; j++)
                 maxArea = Math.Max(maxArea, Math.Min(heights[i], heights[j]) * j - i);
+
+        return maxArea;
+    }
+
+    public static int MaxAreaTwoPointers(int[] heights)
+    {
+        if (heights is null || heights.Length == 0) return 0;
+
+        int left = 0, right = heights.Length - 1;
+        int maxArea = 0;
+        while (right > left)
+        {
+            maxArea = right - left * Math.Min(heights[left], heights[right]);
+            if (heights[left] >= heights[right]) right--;
+            else left++;
+        }
 
         return maxArea;
     }
