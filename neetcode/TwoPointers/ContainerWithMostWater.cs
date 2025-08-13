a3d3bc375d72ed5093e1f9f22165696717c06f19
zkrakus <zkrakus@gmail.com>
2025-08-13 14:22:40 -0400

    feat:container with most rain water brute force.

---
+++ neetcode/TwoPointers/ContainerWithMostWater.cs
@@ -0,0 +1,15 @@
+﻿namespace neetcode.TwoPointers;
+public static class ContainerWithMostWater
+{
+    public static int MaxArea(int[] heights)
+    {
+        if (heights is null || heights.Length == 0) return 0;
+
+        int maxArea = 0;
+        for (int i = 0; i < heights.Length; i++)
+            for (int j = i + 1; j < heights.Length; j++)
+                maxArea = Math.Max(maxArea, Math.Min(heights[i], heights[j]) * j - i);
+
+        return maxArea;
+    }
+}
