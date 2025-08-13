84aacc92165d9bb6c11e28277754f0449d3176df
zkrakus <zkrakus@gmail.com>
2025-08-13 13:54:48 -0400

    feat:GenerateParenthesis Brute force.

--- neetcode/Stack/GenerateParenthesis.cs
+++ neetcode/Stack/GenerateParenthesis.cs
@@ -1,10 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 
 namespace neetcode.Stack;
-internal class GenerateParenthesis
+public static class GenerateParenthesis
 {
-}
+    public static List<string> GenerateParenthesisBruteForce(int n)
+    {
+        List<string> res = new();
+
+        bool ValidParenthesisString(string s)
+        {
+            int open = 0;
+            foreach (char c in s)
+            {
+                open += open == '(' ? 1 : -1;
+                if (open < 0) return false;
+            }
+
+            return open == 0;
+        }
+
+        void DfsGenerateValidParenthesis(string s)
+        {
+            if (s.Length == n * 2)
+            {
+                if (ValidParenthesisString(s))
+                {
+                    res.Add(s);
+                    return;
+                }
+            }
+            DfsGenerateValidParenthesis(s + '(');
+            DfsGenerateValidParenthesis(s + ')');
+        }
+
+        DfsGenerateValidParenthesis("");
+        return res;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
