865f59210170041bac9597619d9024e8dc38b22a
zkrakus <zkrakus@gmail.com>
2025-08-25 15:08:17 -0400

    feat:RemoveNodeFromEndOfLInkedList simpler solution.

--- neetcode/LinkedList/RemoveNodeFromEndOfLInkedList.cs
+++ neetcode/LinkedList/RemoveNodeFromEndOfLInkedList.cs
@@ -1,32 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace neetcode.LinkedList;
+﻿namespace neetcode.LinkedList;
 public static class RemoveNodeFromEndOfLInkedList
 {
     public static ListNode? RemoveNthFromEnd(ListNode? head, int n)
     {
-        if (head is null || head?.next is null)
-            return head;
+        if (head is null || n <= 0) return head;
 
-        ListNode? g = head;
-        ListNode? prevNthNode = null;
+        ListNode? fast = head;
         for (int i = 0; i < n; i++)
         {
-            prevNthNode = g;
-            g = g.next;
-
+            if (fast is null) return head; // n > length
+            fast = fast.next;
         }
 
-        ListNode l = head;
-        while (g.next != null)
+        // If head has to be removed.
+        if (fast is null) 
+            return head.next;
+
+        ListNode? prev = head; // We start at head and work with fast.next != null so we don't have to maintain a pointer behind cur so it's of no interest.
+        while (fast.next != null)
         {
-            g = g.next;
-            l = l.next;
+            prev = prev.next;
+            fast = fast.next;
         }
+
+        prev!.next = prev.next?.next;
 
         return head;
     }
