7911dedb1e5a647bc4d802c9e46cae234693b6cc
zkrakus <zkrakus@gmail.com>
2025-08-27 16:36:59 -0400

    feat:Did RightSideViewDepthFirstSearch ... a little tooo quickly. :)

---
+++ neetcode/Trees/BinaryTreeRightSideView.cs
@@ -0,0 +1,29 @@
+﻿namespace neetcode.Trees;
+public static class BinaryTreeRightSideView
+{
+    public static List<int> RightSideViewDepthFirstSearch(TreeNode root)
+    {
+        List<int> res = new();
+
+        if (root == null)
+            return res;
+
+        void Dfs(TreeNode? cur, int level)
+        {
+            if (cur is null)
+                return;
+
+            if (level == res.Count)
+            {
+                res.Add(cur.val);
+            }
+
+            Dfs(cur.right, level + 1);
+            Dfs(cur.left, level + 1);
+        }
+
+        Dfs(root, 0);
+
+        return res;
+    }
+}
