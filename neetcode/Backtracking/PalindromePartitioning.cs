0f91f3ec7e8b3ab44360ebd3a46802537c571ff0
zkrakus <zkrakus@gmail.com>
2025-08-30 18:44:32 -0400

    feat:Did palindrom partitioning but should also do dynamic programming solution.

---
+++ neetcode/Backtracking/PalindromePartitioning.cs
@@ -0,0 +1,49 @@
+﻿namespace neetcode.Backtracking;
+public static class PalindromePartitioning
+{
+    public static List<List<string>> PartitionBruteForce(string s)
+    {
+        var res = new List<List<string>>();
+        if (s is null || s.Length == 0)
+            return res;
+
+        bool IsPalindrome(int l, int r)
+        {
+            while (r > l)
+            {
+                if (s[r] != s[l])
+                    return false;
+
+                r--;
+                l++;
+            }
+
+            return true;
+        }
+
+        List<string> part = new();
+        void DfsPartitionByPalindrome(int i, int j)
+        {
+            if (j >= s.Length)
+            {
+                if (i == j)
+                    res.Add(part);
+
+                return;
+            }
+
+            if(IsPalindrome(i, j))
+            {
+                part.Add(s.Substring(i, j - i + 1));
+                DfsPartitionByPalindrome(j + 1, j + 1);
+                part.RemoveAt(part.Count - 1);
+            }
+
+            DfsPartitionByPalindrome(i, j + 1);
+        }
+
+        DfsPartitionByPalindrome(0, 0);
+
+        return res;
+    }
+}
