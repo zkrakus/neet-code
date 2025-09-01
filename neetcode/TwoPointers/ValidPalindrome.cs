15eaadf7c9020aaa81c6a4e12a6ed6cb8f3b55d3
zkrakus <zkrakus@gmail.com>
2025-09-01 10:57:59 -0400

    feat:Super fast valid palindrome that i missed.

---
+++ neetcode/TwoPointers/ValidPalindrome.cs
@@ -0,0 +1,22 @@
+﻿namespace neetcode.TwoPointers;
+internal static class ValidPalindrome
+{
+    public static bool IsPalindrome(string s)
+    {
+        if (s is null)
+            return false;
+
+        int l = 0, r = s.Length - 1;
+
+
+        while (r > l) 
+        {
+            if (s[l] != s[r])
+                return false;
+            r--;
+            l++;
+        }
+
+        return true;
+    }
+}
