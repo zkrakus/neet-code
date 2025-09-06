02665592ecdd2376f13d0d96a7c894f6cafddf8e
zkrakus <zkrakus@gmail.com>
2025-09-06 12:06:27 -0400

    feat:Finished easy tree problem (medium) but it's just tree traveral ... might want to try more practice with DFS and try returning state from recursive functions over void to prevent closing over state.

---
+++ neetcode/Trees/CountGoodNodesInBinaryTree.cs
@@ -0,0 +1,29 @@
+﻿namespace neetcode.Trees;
+public static class CountGoodNodesInBinaryTree
+{
+    public static int GoodNodes(TreeNode root)
+    {
+        int count = 0;
+        if (root == null) 
+            return count;
+
+
+        int DfsTreeCount(TreeNode? node, int max = int.MinValue)
+        {
+            if (node == null)
+                return 0 ;
+
+            var res = node.val >= max ? 1 : 0;
+            max = Math.Max(max, node.val);
+
+            res += DfsTreeCount(node?.left, max);
+            res += DfsTreeCount(node?.right, max);
+
+            return res;
+        }
+
+        count = DfsTreeCount(root);
+
+        return count;
+    }
+}
