2f6001d915687fe07a503e356919757dbb5bae32
zkrakus <zkrakus@gmail.com>
2025-09-08 11:35:43 -0400

    feat:Copy more copy list alterations just to get all the traversal edge cases down.

---
+++ neetcode/LinkedList/CopyList.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using System.Runtime.ConstrainedExecution;
+
+namespace neetcode.LinkedList;
+public static class CopyList
+{
+    // t: O(n)
+    // S: O(1)
+    public static ListNode? CopyDirty(ListNode? root)
+    {
+        ListNode? head = root, newHead = null!, cur = root, prev = null!;
+        while (cur != null)
+        {
+            ListNode copy = new(cur.val);
+            if (newHead is null)
+            {
+                newHead = copy;
+                prev = copy;
+            }
+            else
+            {
+                prev.next = copy;
+                prev = prev.next;
+            }
+
+            cur = cur.next;
+        }
+
+        return newHead;
+    }
+
+    // t: O(n) :: recursive function | for each node in we copy.
+    // s: O(t) :: n pushes to the stack for recursive calls.
+    public static ListNode? CopyRecursion(ListNode? root)
+    {
+        if (root is null)
+            return null!;
+
+        return new ListNode(root.val)
+        {
+            next = CopyRecursion(root.next)
+        };
+    }
+
+    // t: O(n) :: Iterate though LL, we just handle the first iteration explicitly outside the loop.
+    // s: O(t) :: n pushes to the stack for recursive calls.
+    public static ListNode? CopyHandleHeadFirst(ListNode? root)
+    {
+        if (root is null)
+            return null!;
+
+        ListNode? newHead = new(root.val), tail = newHead, cur = root.next; // Handle first iteration inline.
+        while (cur != null)
+        {
+            tail.next = new(cur.val);
+            cur = cur.next;
+            tail = tail.next;
+        }
+
+        return newHead;
+    }
+}
