d2e41a117273f7b125b444e7af847417ed58b8f1
zkrakus <zkrakus@gmail.com>
2025-09-07 20:15:23 -0400

    feat: easy one.

---
+++ neetcode/ArraysAndHashing/ConcatentationOfAnArray.cs
@@ -0,0 +1,21 @@
+﻿namespace neetcode.ArraysAndHashing;
+public static class ConcatentationOfAnArray
+{
+    public static int[]? GetConcatenation(int[] nums)
+    {
+        if (nums is null)
+            return null!;
+
+        if (nums.Length == 0)
+            return Array.Empty<int>();
+        
+        var ans = new int[nums.Length * 2];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            ans[i] = nums[i];
+            ans[nums.Length + i] = nums[i];
+        }
+
+        return ans;
+    }
+}
