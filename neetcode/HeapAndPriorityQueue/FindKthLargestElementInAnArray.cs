ff8a094979b3febede7f243de0c48098cf02daa9
zkrakus <zkrakus@gmail.com>
2025-09-12 10:53:32 -0400

    feat:Kth largest element in an array with a PK.

---
+++ neetcode/HeapAndPriorityQueue/FindKthLargestElementInAnArray.cs
@@ -0,0 +1,19 @@
+﻿namespace neetcode.HeapAndPriorityQueue;
+
+public class FindKthLargestElementInAnArray
+{
+    // t(n): O(n log(k))
+    // s(n): O(k);
+    public int FindKthLargest(int[] nums, int k)
+    {
+        PriorityQueue<int, int> minHeap = new PriorityQueue<int, int>(Comparer<int>.Create((a, b) => a.CompareTo(b)));
+        foreach (int num in nums)
+        {
+            minHeap.Enqueue(num, num);
+            if(minHeap.Count > k)
+                minHeap.Dequeue();
+        }
+
+        return minHeap.Peek();
+    }
+}
