32d7e038e0edcfb99b5291d53c74c7821f9ca83b
zkrakus <zkrakus@gmail.com>
2025-10-01 13:48:47 -0400

    feat:Added middleout solution for longest palindromic substring.

--- neetcode/OneDimensionalDynamicProgramming/LongestPalindromicSubstring.cs
+++ neetcode/OneDimensionalDynamicProgramming/LongestPalindromicSubstring.cs
@@ -1,4 +1,6 @@
-﻿namespace neetcode.OneDimensionalDynamicProgramming;
+﻿using System.ComponentModel;
+
+namespace neetcode.OneDimensionalDynamicProgramming;
 public static class LongestPalindromicSubstring
 {
     public static string LongestPalindromeBrute(string s)
@@ -22,6 +24,39 @@ public static class LongestPalindromicSubstring
 
         return s.Substring(largestLeftIndex, largestPalindromeLength);
     }
+
+    public static string LongestPalindromicSubstringMiddleOut(string s)
+    {
+        if (s is null || s.Length == 0)
+            return "";
+
+        int largestLeftIndex = 0;
+        int largestPaliLength = 1;
+        void IsPalindromeExpansion(int l, int r)
+        {
+            while (l >= 0 && r < s.Length && (s[l] == s[r]))
+            {
+                var palindromicLength = r - l + 1;
+                if (palindromicLength > largestPaliLength)
+                {
+                    largestLeftIndex = l;
+                    largestPaliLength = palindromicLength;
+                }
+
+                l--;
+                r++;
+            }
+        }
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            IsPalindromeExpansion(i, i);
+            IsPalindromeExpansion(i, i + 1);
+        }
+
+        return s.Substring(largestLeftIndex, largestPaliLength);
+    }
+
     private static bool IsPalindrome(string s, int l, int r)
     {
         while (l < r)
