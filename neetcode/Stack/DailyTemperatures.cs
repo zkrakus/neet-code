6665053b27cecd67f02de6b73488b1bf772a931b
zkrakus <zkrakus@gmail.com>
2025-10-06 09:09:11 -0400

    feat:Did daily temperatures. Mediums are starting to feel easy.

--- neetcode/Stack/DailyTemperatures.cs
+++ neetcode/Stack/DailyTemperatures.cs
@@ -1,27 +1,20 @@
 ﻿namespace neetcode.Stack;
-internal class DailyTemperatures
+public static class DailyTemperatures
 {
-    public static string Solve(int[] temperatures)
+    public static int[] Solve(int[] temperatures)
     {
-        Stack<(int, int)> tempStack = new();
+        Stack<(int value, int index)> tempStack = new();
         var result = new int[temperatures.Length];
         for (int i = 0; i < temperatures.Length; i++)
         {
-            if (i == temperatures.Length - 1)
-                result[i] = 0;
-
             var curTemp = temperatures[i];
-            if (tempStack.Count != 0 && tempStack.Peek().Item1 < curTemp)
+            while (tempStack.Count != 0 && tempStack.Peek().value < curTemp)
             {
-                (int value, int index) = tempStack.Pop();
-                result[index] = index - i;
+                ( _ , int prevIndex) = tempStack.Pop();
+                result[prevIndex] = i - prevIndex;
             }
             
             tempStack.Push((curTemp, i));
-            if (curTemp  )
-            {
-
-            }
         }
 
         return result;
