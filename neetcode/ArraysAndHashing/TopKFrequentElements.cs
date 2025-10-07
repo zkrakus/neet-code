8c027a53ef6b90f3faa1227a46065ea797faa868
zkrakus <zkrakus@gmail.com>
2025-10-07 19:08:22 -0400

    feat:Did another version of top k elements. and fixed my old one.

--- neetcode/ArraysAndHashing/TopKFrequentElements.cs
+++ neetcode/ArraysAndHashing/TopKFrequentElements.cs
@@ -1,18 +1,46 @@
 ﻿namespace neetcode.ArraysAndHashing;
 public static class TopKFrequentElements
-{
-    public static int[] TopKFrequent(int[] nums, int k)
+{ 
+    // f(t) : O(n*log(n) + log(k)) 
+    // f(s) : O(k)
+    public static int[] TopKFrequentMaxHeap(int[] nums, int k)
     {
         Dictionary<int, int> elementCount = new();
-        foreach (int i in nums)
-            elementCount[i] = elementCount.GetValueOrDefault(i) + 1;
+        foreach (int num in nums)
+            elementCount[num] = elementCount.GetValueOrDefault(num) + 1;
 
-        PriorityQueue<int, int> minHeap = new(Comparer<int>.Create((a,b) => b.CompareTo(a)));
-        foreach (var element in elementCount.Keys)
-            minHeap.Enqueue(element, elementCount[element]);
+        PriorityQueue<int, int> maxHeap = new PriorityQueue<int, int>(Comparer<int>.Create((a, b) => b.CompareTo(a)));
+        foreach(var key in elementCount.Keys)
+                maxHeap.Enqueue(key, elementCount[key]);
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++)
+            result[i] = maxHeap.Dequeue();
+
+        return result;
+    }
+
+
+
+    // f(t) : O(n + n*log(k) ) 
+    // f(s) : O(n + k)
+    public static int[] TopKFrequentMinHeap(int[] nums, int k)
+    {
+        Dictionary<int, int> elementCount = new();
+        foreach (int num in nums)
+            elementCount[num] = elementCount.GetValueOrDefault(num) + 1;
+
+        PriorityQueue<int, int> minHeap = new PriorityQueue<int, int>(Comparer<int>.Create((a, b) => a.CompareTo(b)));
+        foreach (var key in elementCount.Keys)
+        {
+            minHeap.Enqueue(key, elementCount[key]);
+            
+            if (minHeap.Count > k)
+                minHeap.Dequeue();
+        }
 
         var result = new int[k];
-        for (int i = 0; i < k; i++)
+        for(int i = 0; i < k; i++)
             result[i] = minHeap.Dequeue();
 
         return result;
